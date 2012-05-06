75864503490abafdd2e06df20cd07ac77abdd3f3
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-05-06 22:24:39 +0300

    skip issues and meeting in case of a http error

--- fetch.py
+++ fetch.py
@@ -2,6 +2,7 @@ import errno
 import os
 import os.path
 import sys
+from urllib.error import HTTPError
 from urllib.parse import urljoin
 from urllib.parse import urlsplit
 from urllib.request import urlopen
@@ -15,8 +16,13 @@ def _main():
     soup = bs4.BeautifulSoup(urlopen(base_url), from_encoding="windows-1252")
     index_urls = [urljoin(base_url, h3("a")[0]["href"]) for h3 in soup("h3")]
     for index_url in index_urls:
-        index_soup = bs4.BeautifulSoup(urlopen(index_url),
-                                       from_encoding="iso-8859-1")
+        try:
+            resp = urlopen(index_url)
+        except HTTPError as err:
+            print(err, err.url, file=sys.stderr)
+            print("Skipping..", file=sys.stderr)
+            continue
+        index_soup = bs4.BeautifulSoup(resp, from_encoding="iso-8859-1")
         clean_index_soup = klupu.clean_soup(index_soup)
         index_path = urlsplit(index_url).path
         index_filepath = os.path.normpath("." + index_path)
@@ -29,8 +35,13 @@ def _main():
             f.write(clean_index_soup.prettify())
         for issue_url in klupu.iter_issue_urls(clean_index_soup):
             issue_url = urljoin(index_url, issue_url)
-            issue_soup = bs4.BeautifulSoup(urlopen(issue_url),
-                                           from_encoding="windows-1252")
+            try:
+                resp = urlopen(issue_url)
+            except HTTPError as err:
+                print(err, err.url, file=sys.stderr)
+                print("Skipping..", file=sys.stderr)
+                continue
+            issue_soup = bs4.BeautifulSoup(resp, from_encoding="windows-1252")
             clean_issue_soup = klupu.clean_soup(issue_soup)
             issue_path = urlsplit(issue_url).path
             issue_filepath = os.path.normpath("." + issue_path)
