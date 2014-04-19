cdf11f274466edb7eeb647b3281b05f2c0d9fb9b
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-19 13:42:44 +0300

    Provide simple setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from distutils.core import setup
+
+setup(name='klupung',
+      version='0.1.0',
+      description='Klupu Next Generation',
+      author='Tuomas Räsänen',
+      author_email='tuomasjjrasanen@tjjr.fi',
+      license='AGPLv3+',
+      # package_dir={'klupung': 'lib'},
+      packages=['klupung'],
+      scripts=["bin/klupung-download-meetingdoc",
+               "bin/klupung-stupid-apiserver",
+               "bin/klupung-db-create",
+               "bin/klupung-db-import-meetingdoc",
+               "bin/klupung-db-insert-categories",
+               "bin/klupung-db-insert-policymaker"],
+      platforms=['Linux'],
+)
