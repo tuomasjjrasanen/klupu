e65749423c4941b39d1c2ada915ab7747bb11953
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-09-16 23:09:15 +0300

    deployment/jkl: fix refresh script

--- deployment/jkl/refresh.sh
+++ deployment/jkl/refresh.sh
@@ -42,7 +42,22 @@ fi
 G_this_script_path=$(readlink -e "$0")
 G_this_script_dir=$(dirname "${G_this_script_path}")
 
-"{G_this_script_dir}/download.sh"
-"{G_this_script_dir}/stop.sh"
-"{G_this_script_dir}/dbimport.sh"
-"{G_this_script_dir}/start.sh"
+# Download failure is not fatal. jyvaskyla.fi might be down for example,
+# just keep going because we might have just downloaded at least some
+# new documents.
+echo "Downloading new documents..."
+"${G_this_script_dir}/download.sh" || true
+
+echo "Stopping klupung..."
+"${G_this_script_dir}/stop.sh" || {
+    # Stop failure is fatal, but try to start the service before dying.
+    echo "Starting klupung..."
+    "${G_this_script_dir}/start.sh"
+    exit 1
+}
+
+echo "Importing documents into db..."
+"${G_this_script_dir}/dbimport.sh" || true
+
+echo "Starting klupung..."
+"${G_this_script_dir}/start.sh"
